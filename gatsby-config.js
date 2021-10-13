5aef73ae004dff571c6315a9373b5e5b1984a52f
klporter1219 <78804731+klporter1219@users.noreply.github.com>
2021-10-13 17:00:59 -0400

    Changes to nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://in-info-web4.informatics.iupui.edu/~klsipes/kp-midterm/",
+    siteUrl: "https://in-info-web4.informatics.iupui.edu/~klsipes/kp-midterm",
     title: "kp-midterm",
   },
   plugins: [],
+  pathPrefix: "~klsipes/kp-midterm"
 };
